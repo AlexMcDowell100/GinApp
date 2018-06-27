59c6a8ba12ee35efedc1bacd90ea1680871fcd66
AlexMcDowell100 <amcdowell100@hotmail.com>
2018-06-27 11:42:51 +0100

    moved the text in application rb

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 
-config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
+
 
 require_relative 'boot'
 
@@ -14,6 +14,7 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.paths << "#{Rails.root}/app/assets/videos"
+    config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
